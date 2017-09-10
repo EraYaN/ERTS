70b5e7616b1b6934d5dc3dac50843c0cf14813ae
Erwin de Haan <EraYaN@users.noreply.github.com>
2017-09-10 17:53:03 +0200

    Made the solution build again. TODO protocl methods and all the datastructures.

--- erts-pc/ERTSDashboard/Communication/CommunicationInterface.cs
+++ erts-pc/ERTSDashboard/Communication/CommunicationInterface.cs
@@ -1,12 +1,10 @@
 ﻿using EraYaN.Serial;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Linq;
 
-namespace ERTS.Dashboard.Communication {
+namespace ERTS.Dashboard.Communication
+{
     public class CommunicationInterface : IDisposable {
         SerialInterface serial;
         string serialPort;
@@ -16,7 +14,6 @@ namespace ERTS.Dashboard.Communication {
         bool isReceivingPacket = false;
         int bufferIndex = 0;
         byte[] packetBuffer = new byte[Packet.DATA_SIZE];
-        int expectedPacketLength = Packet.MAX_PACKET_SIZE;
 
         #region Status Properties
         public bool IsOpen {
@@ -38,55 +35,60 @@ namespace ERTS.Dashboard.Communication {
             serial = new SerialInterface(SerialPort, BaudRate);
             serial.SerialDataEvent += com_SerialDataEvent;
             if (serial.OpenPort() != 0) {
-                System.Diagnostics.Debug.WriteLine(serial.lastError);
+                Debug.WriteLine(serial.lastError);
             }
         }
 
         void com_SerialDataEvent(object sender, SerialDataEventArgs e) {            
-            if (isReceivingPacket == false) {                
-                if (e.Data == 0xF0) {
-                    GlobalData.db.UpdateProperty("SerialPortStatusColor");
-                    GlobalData.db.UpdateProperty("SerialPortStatus");
-                    packetBuffer = new List<byte>();
-                    isReceivingPacket = true;
-                    expectedPacketLength = 4;
-                    packetBuffer.Add(e.Data);
-                } else {
-                    isReceivingPacket = false;
-                }
+            if (isReceivingPacket == false) {
+                isReceivingPacket = true;
+                packetBuffer[bufferIndex] = e.Data;
+                bufferIndex++;                
             } else {
-                if (e.Data == 0xF0 && packetBuffer.Count == 1) {
-                    System.Diagnostics.Debug.WriteLine("Second byte is also start byte.");
-                } else {
-                    packetBuffer.Add(e.Data);
-                }
-                if (packetBuffer.Count == 3) {
-                    //command received
-                    expectedPacketLength = ((MessageType)packetBuffer[2]).BasePacketLength();
-                }
-                if (packetBuffer.Count == 6) {
-
-                    if (((MessageType)packetBuffer[2]).HasNData()) {
-                        int datlen = (short)((packetBuffer[5] << 8) + packetBuffer[4]);
-                        expectedPacketLength = 6 + datlen;
+                packetBuffer[bufferIndex] = e.Data;
+                bufferIndex++;
+                if (bufferIndex == 2) // Received two bytes, check starting sequence
+                {
+                    if(packetBuffer[0]!=((Packet.START_SEQUENCE & 0xFF00) >> 8) || packetBuffer[1] != ((Packet.START_SEQUENCE & 0x00FF)))
+                    {
+                        //TODO Send exception
+                        isReceivingPacket = false;
+                        bufferIndex = 0;
                     }
                 }
-            }
-
-            if (isReceivingPacket) {
-                if (packetBuffer.Count >= expectedPacketLength) {
-                    try {
-                        PacketReceived(new Packet(packetBuffer.ToArray()));
-                    } catch (ArgumentException ex) {
-                        //Bad Packet, Discard
-                        System.Diagnostics.Debug.WriteLine(ex);
+                if (bufferIndex == 3) // Received three bytes, check MessageType
+                {
+                    if (!Enum.IsDefined(typeof(MessageType), packetBuffer[2]))
+                    {
+                        //TODO Send exception
+                        isReceivingPacket = false;
+                        bufferIndex = 0;
                     }
-                    isReceivingPacket = false;
-                    packetBuffer.Clear();
+                }
+                if (bufferIndex == Packet.MAX_PACKET_SIZE) // Received twenty bytes, check endsequence
+                {
+                    if (packetBuffer[Packet.MAX_PACKET_SIZE-1] != Packet.END_SEQUENCE)
+                    {
+                        //TODO Send exception
+                        isReceivingPacket = false;
+                        bufferIndex = 0;
+                    } else
+                    {
+                        try
+                        {
+                            PacketReceived(new Packet(packetBuffer.ToArray()));
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            //Bad Packet, Discard
+                            System.Diagnostics.Debug.WriteLine(ex);
+                        }
+                        isReceivingPacket = false;
+                        bufferIndex = 0;
+                    }
                 }
 
-            }
-
+            }  
         }
 
         public void SendPacket(Packet p) {
@@ -96,183 +98,15 @@ namespace ERTS.Dashboard.Communication {
         }
 
         #region Protocol Methods
-        public void OK() {
+        /*public void OK() {
             Packet p = new Packet();
             p.Type = MessageType.OK;
             p.ID = 0;
             p.DataLength = 0;
             p.Data = new byte[0] { };
             SendPacket(p);
-        }
-        public void Reset() {
-            Packet p = new Packet();
-            p.Type = MessageType.Reset;
-            p.ID = 0;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void Refresh() {
-            Packet p = new Packet();
-            p.Type = MessageType.Refresh;
-            p.ID = 0;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void Forbidden(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.Forbidden;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void NotFound(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.NotFound;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void TypeMismatch(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.TypeMismatch;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void VersionUnsupported() {
-            Packet p = new Packet();
-            p.Type = MessageType.VersionUnsupported;
-            p.ID = 0;
-            p.DataLength = 1;
-            p.Data = new byte[1] { PROTOCOL_VERSION };
-            SendPacket(p);
-        }
-        public void Unsupported() {
-            Packet p = new Packet();
-            p.Type = MessageType.Unsupported;
-            p.ID = 0;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void SetBoolean(byte id, bool val) {
-            Packet p = new Packet();
-            p.Type = MessageType.SetBoolean;
-            p.ID = id;
-            p.DataLength = sizeof(byte);
-            p.Data = new byte[1] { (val ? (byte)1 : (byte)0) };
-            SendPacket(p);
-
-        }
-        public void SetInteger16(byte id, short val) {
-            Packet p = new Packet();
-            p.Type = MessageType.SetInteger16;
-            p.ID = id;
-            p.DataLength = sizeof(short);
-            p.Data = BitConverter.GetBytes(val);
-            SendPacket(p);
-        }
-        public void SetInteger32(byte id, int val) {
-            Packet p = new Packet();
-            p.Type = MessageType.SetInteger32;
-            p.ID = id;
-            p.DataLength = sizeof(int);
-            p.Data = BitConverter.GetBytes(val);
-            SendPacket(p);
-        }
-        public void SetFloat(byte id, float val) {
-            Packet p = new Packet();
-            p.Type = MessageType.SetFloat;
-            p.ID = id;
-            p.DataLength = sizeof(float);
-            p.Data = BitConverter.GetBytes(val);
-            SendPacket(p);
-        }
-        public void SetString(byte id, string val) {
-            byte[] str_b = Encoding.ASCII.GetBytes(val);
-            Packet p = new Packet();
-            p.Type = MessageType.SetString;
-            p.ID = id;
-            p.DataLength = (short)str_b.Length;
-            p.Data = str_b;
-            SendPacket(p);
-        }
-
-        public void SetFloatArray(byte id, float[] val) {
-            byte[] arr_b = new byte [val.Length*sizeof(float)];
-            for (int i = 0; i < val.Length; i++) {
-                Buffer.BlockCopy(BitConverter.GetBytes(val[i]), 0, arr_b, i*sizeof(float), sizeof(float));
-            }
-            Packet p = new Packet();
-            p.Type = MessageType.SetFloatArray;
-            p.ID = id;
-            p.DataLength = (short)arr_b.Length;
-            p.Data = arr_b;
-            SendPacket(p);
-        }
-
-        public void GetBoolean(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.GetBoolean;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void GetInteger16(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.GetInteger16;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void GetInteger32(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.GetInteger32;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void GetFloat(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.GetFloat;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void GetString(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.GetString;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void GetFloatArray(byte id) {
-            Packet p = new Packet();
-            p.Type = MessageType.GetFloatArray;
-            p.ID = id;
-            p.DataLength = 0;
-            p.Data = new byte[0] { };
-            SendPacket(p);
-        }
-        public void Debug(string val) {
-            byte[] str_b = Encoding.ASCII.GetBytes(val);
-            Packet p = new Packet();
-            p.Type = MessageType.Debug;
-            p.ID = 0;
-            p.DataLength = (short)str_b.Length;
-            p.Data = str_b;
-            SendPacket(p);
-        }
+        }*/
+        
         #endregion
 
         #region IDisposable members
