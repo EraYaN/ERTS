71e684781e73d4590486af8f6570710efa219674
Erwin de Haan <EraYaN@users.noreply.github.com>
2017-09-27 11:01:59 +0200

    Fake Embedded changes.

--- src/ERTS.Embedded/drivers/fake/serial.h
+++ src/ERTS.Embedded/drivers/fake/serial.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <string>
+#if defined(_WIN32) || defined(_WIN64)
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
 
 class Serial {
 
-#if defined(_WIN32) || defined(_WIN64)
-#include <windows.h>
 
+#if defined(_WIN32) || defined(_WIN64)
     HANDLE handle;
 #else
     int handle;
@@ -22,4 +25,6 @@ public:
     bool getchar_nb(char *c);
 
     int putchar(char c);
+
+	int Serial::read(char *buffer, int buffLen, bool nullTerminate);
 };
